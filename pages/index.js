e523206cb0082fdad6b65e516f502fb0dcb899bb
Pedro <pedropmun@gmail.com>
2020-06-15 22:25:42 -0300

    removing unused import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import HomeScreen from "@screens/Home";
-import { fetchData } from "@services/fetch";
 
 const Index = ({ chartData }) => {
   return <HomeScreen chartData={chartData} />;
